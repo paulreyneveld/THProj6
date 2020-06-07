0e1b73a72a084d35e01d842011ffcc05bdfffdbf
Paul <63985305+paulreyneveld@users.noreply.github.com>
2020-06-07 14:18:00 -0700

    Route addresses appropriately

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const JSONdata = require('./data.json');
+
+const app = express();
+
+app.use('/static', express.static('public'));
+app.set('view engine', 'pug');
+
+const mainRoutes = require('./routes');
+const projectRoutes = require('./routes/projects');
+app.use(mainRoutes);
+app.use('/projects', projectRoutes);
+
+app.listen(3000, () => {
+	console.log('This app is running on port 3000!');
+});
