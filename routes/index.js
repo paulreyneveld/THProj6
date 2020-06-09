b9b67c24e29b726f3c397891fb0ed766fa473125
Paul <63985305+paulreyneveld@users.noreply.github.com>
2020-06-09 16:26:48 -0700

    Added images folder and updated profile info

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const data = require('../data.json')
 
 router.get('/', (req, res) => {
-        res.render('index');
+	const aboutLink = '/about';
+	res.render('index');
 });
 
 router.get('/about', (req, res) => {
         res.render('about');
+});
+
+router.get('/:id', (req, res) => {
+	const { id } = req.params;
+	const title = data.projects[id].project_name;
+	const description = data.projects[id].description;
+	const technologies = data.projects[id].technologies;
+	const github_link = data.projects[id].github_link;
+	const projData = { id, title, description, technologies, github_link };
+
+	res.render('project', projData);
 });
 
 module.exports = router;
